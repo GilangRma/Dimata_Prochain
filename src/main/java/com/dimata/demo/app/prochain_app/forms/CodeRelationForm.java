e1d255fccdce99d336e2e6cdc4be9a6e071a833a
pius11 <96400519+pius11@users.noreply.github.com>
2022-04-04 11:36:59 +0800

    merapikan

---
+++ src/main/java/com/dimata/demo/app/prochain_app/forms/CodeRelationForm.java
@@ -0,0 +1,10 @@
+package com.dimata.demo.app.prochain_app.forms;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CodeRelationForm {
+    public String code;
+}
--- src/main/java/com/dimata/demo/app/prochain_app/models/table/PosPriceTypeMapping.java
+++ src/main/java/com/dimata/demo/app/prochain_app/models/table/PosPriceTypeMapping.java
@@ -8,12 +8,12 @@ import java.util.Objects;
 import com.dimata.demo.app.prochain_app.core.api.UpdateAvailable;
 import com.dimata.demo.app.prochain_app.core.util.GenerateUtil;
 import com.dimata.demo.app.prochain_app.core.util.ManipulateUtil;
-import com.dimata.demo.app.prochain_app.core.util.jackson.TimeDeserialize;
+
 import com.dimata.demo.app.prochain_app.core.util.jackson.TimeSerialize;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
